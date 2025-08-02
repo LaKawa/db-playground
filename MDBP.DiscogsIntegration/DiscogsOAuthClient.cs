44439f6da22efd316a0d49ae69efdce498274150
Lukas Sziele <lukassziele@aol.de>
2025-08-02 23:12:55 +0200

    Add Discogs-specific OAuth validation variables
    Includes all needed Urls for authorization
    OAuthSignatureMethod.PLAINTEXT enum needs to be added in OAuth class lib

---
+++ MDBP.DiscogsIntegration/DiscogsOAuthClient.cs
@@ -0,0 +1,26 @@
+﻿using OAuth.OAuth1;
+
+namespace MusicDBPlayground.DiscogsIntegration;
+
+public class DiscogsOAuthClient(HttpClient httpClient)
+{
+    private const string RequestTokenUrl = "https://api.discogs.com/oauth/request_token";
+    private const string AccessTokenUrl = "https://api.discogs.com/oauth/access_token";
+    private const string AuthorizeUrl = "https://www.discogs.com/oauth/authorize";
+
+    private readonly OAuth1Client _authClient = new OAuth1Client(
+        httpClient,
+        Environment.GetEnvironmentVariable("DISCOGS_CONSUMER_KEY")
+            ?? throw new InvalidOperationException("DISCOGS_CONSUMER_KEY isn't set as environment variable!"),
+        Environment.GetEnvironmentVariable("DISCOGS_CONSUMER_SECRET")
+            ?? throw new InvalidOperationException("DISCOGS_CONSUMER_SECRET is not set as environment variable!"),
+        RequestTokenUrl,
+        AccessTokenUrl,
+        AuthorizeUrl,
+        OAuthSignatureMethod.PLAINTEXT);
+    
+    public async Task<string> GetRequestTokenAsync()
+    {
+        throw new NotImplementedException();
+    } 
+}
