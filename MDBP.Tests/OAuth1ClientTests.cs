98e8930801b3a7c7c0d469472ecfbcf840976814
Lukas Sziele <lukassziele@aol.de>
2025-08-05 00:13:25 +0200

    Finish OAuth header creation & test
    Added nonce and timestamp part to header string
    Create xunit tests, locally the header string looks correct

---
+++ MDBP.Tests/OAuth1ClientTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using OAuth.OAuth1;
+
+namespace MDBP.Tests;
+
+public class OAuth1ClientTests
+{
+    [Fact]
+    public void BuildAuthorizationHeader_Plaintext_ReturnsHeader()
+    {
+        var client = new HttpClient();
+        var oAuthClient = new OAuth1Client(
+            client,
+            "testKey",
+            "testSecret",
+            "https://api.discogs.com/oauth/request_token",
+            "https://api.discogs.com/oauth/access_token",
+            "https://www.discogs.com/oauth/authorize",
+            "http://localhost:8976/callback",
+            OAuthSignatureMethod.PLAINTEXT);
+
+        var header = oAuthClient.BuildAuthorizationHeader(
+            token: null,
+            tokenSecret: null,
+            callbackUrl: "http://localhost:8976/callback",
+            verifier: null,
+            httpMethod: "GET",
+            url: "https://api.discogs.com/oauth/request_token");
+        
+        Assert.Contains("oauth_signature_method=\"PLAINTEXT\"", header);
+        Assert.Contains("oauth_consumer_key=\"testKey\"", header);
+        Assert.Contains($"oauth_signature=\"{Uri.EscapeDataString("testSecret&")}\"", header);
+        Assert.Contains($"oauth_callback=\"{Uri.EscapeDataString("http://localhost:8976/callback")}\"", header);
+    }
+        
+}
